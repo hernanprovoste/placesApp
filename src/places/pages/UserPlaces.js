47287df9466575bfa65e8988643391afa86bef90
Hernán A. Provoste V <hernanprovostev@outlook.com>
2020-08-06 23:51:39 -0400

    Add modal and programming place item

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
 import PlaceList from '../components/PlaceList';
+
 
 const DUMMY_PLACES = [
   {
@@ -24,13 +26,16 @@ const DUMMY_PLACES = [
         lat: 40.7484405,
         lng: -73.9878531
     },
-    creator: 'u1'
+    creator: 'u2'
   }
 ]
 
 const UserPlaces = () => {
+  const userId = useParams().userId
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
   return (
-    <PlaceList items={DUMMY_PLACES} />
+    <PlaceList items={loadedPlaces} />
   )
 }
 
